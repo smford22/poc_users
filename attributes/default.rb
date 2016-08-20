01729c5701fe5c14444f141d7d29646eb64857bf
Chef Software <chef@chef.io>
2016-08-20 03:59:10 +0000

    initial commit

---
+++ attributes/default.rb
@@ -0,0 +1,6 @@
+default['poc_users'] = [ "chef1", "chef2", "chef3",
+                         "chef4", "chef5", "chef6",
+                         "chef7", "chef8", "chef9",
+                         "chef10", "chef11", "chef12",
+                         "chef13", "chef14", "chef15"
+                       ]
--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,17 @@
 # Recipe:: default
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
+users = node['poc_users']
+
+users.each do |u|
+  directory "c:\\Users\\Administrator\\#{u}\\.delivery" do
+    recursive true
+    inherits true
+  end
+
+  template "C:\\Users\\Administrator\\#{u}\\.delivery\\cli.toml" do
+    source 'cli.toml.erb'
+    inherits true
+    variables(:user => u)
+  end
+end
